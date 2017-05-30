f04dd3741ca30fe4030f990fdc875f45c2717f6c
jonas <jregueir@gmail.com>
2017-05-30 12:20:15 +0100

    Preservando cambios por si acaso

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -5,9 +5,9 @@ from . import views
 urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^overview$', TemplateView.as_view(template_name="dashboard/home.html"), name='home'),
-    url(r'^control_led_matrix$', TemplateView.as_view(template_name="dashboard/control_led_matrix.html"), name='led_matrix'),
+    url(r'^control-led-matrix$', TemplateView.as_view(template_name="dashboard/control_led_matrix.html"), name='led_matrix'),
     url(r'^env-sensors$', TemplateView.as_view(template_name="dashboard/env_sensors.html"), name='led_matrix'),
-    url(r'^example_jquery$', TemplateView.as_view(template_name="example-sensehatjs.html"), name='example_jquery'),
+    url(r'^example-jquery$', TemplateView.as_view(template_name="example-sensehatjs.html"), name='example_jquery'),
     url(r'^base$', TemplateView.as_view(template_name="dashboard/ull_base.html"), name='base'),
     url(r'^base2$', TemplateView.as_view(template_name="base_example.html"), name='base'),
 ]
