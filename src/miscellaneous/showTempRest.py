56a09b157a3e430c289dd0353f2adbf626795976
Jonás <jregueir@gmail.com>
2017-12-17 19:56:41 +0000

    Get metric a show message in loop

--- src/miscellaneous/showTempRest.py
+++ src/miscellaneous/showTempRest.py
@@ -7,7 +7,7 @@
 from unipath import Path
 import requests
 import yaml
-import json
+import time
 
 BASE_DIR = Path(__file__).parent
 ymlconfig = BASE_DIR.child("showTempRest.yml")
@@ -27,24 +27,24 @@ def getMetricColour(name, value):
     else:
         return red
 
-
-for metric in ('temperature', 'humidity'):
-    rTemp = requests.get(cfg[metric]['api'], auth=(cfg['user'], cfg['password']))
-    data = rTemp.json()
-    text_colour = getMetricColour(metric, data['result'])
-    if metric == 'temperature':
-        text_string = "T" + str(round(data['result'],1))
-    else:
-        text_string = "H" + str(round(data['result'],1))
-    payload = {
-        'back_colour': {
-            'r': 0,
-            'g': 0,
-            'b': 0
-        },
-        'text_colour': text_colour,
-        'text_string': text_string,
-        'scroll_speed': 0.1
-    }
-    r = requests.put(cfg['show_message']['api'], json=payload, auth=(cfg['user'], cfg['password']))
-
+while True:
+    for metric in ('temperature', 'humidity'):
+        rTemp = requests.get(cfg[metric]['api'], auth=(cfg['user'], cfg['password']))
+        data = rTemp.json()
+        text_colour = getMetricColour(metric, data['result'])
+        if metric == 'temperature':
+            text_string = "T" + str(round(data['result'],1))
+        else:
+            text_string = "H" + str(round(data['result'],1))
+        payload = {
+            'back_colour': {
+                'r': 0,
+                'g': 0,
+                'b': 0
+            },
+            'text_colour': text_colour,
+            'text_string': text_string,
+            'scroll_speed': 0.1
+        }
+        r = requests.put(cfg['show_message']['api'], json=payload, auth=(cfg['user'], cfg['password']))
+    time.sleep(cfg['sleep_loop'])
