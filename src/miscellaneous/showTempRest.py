f595dafbef02440ee554f041a9991f325c48c414
Jonás <jregueir@gmail.com>
2017-12-17 19:35:16 +0000

    It work

--- src/miscellaneous/showTempRest.py
+++ src/miscellaneous/showTempRest.py
@@ -10,44 +10,35 @@ with open(ymlconfig, 'r') as ymlfile:
 
 
 def getMetricColour(name, value):
-    red=(255,0,0)
-    green=(0,255,0)
-    yellow=(255, 255, 0)
+    red = {'r': 255, 'g': 0, 'b': 0}
+    green = {'r': 0, 'g': 255, 'b': 0}
+    yellow= {'r': 255, 'g':255, 'b':0}
 
-    if value <= cfg[name]['green']:
+    if value <= cfg[name]['thresholds']['green']:
         return green
-    elif value <= cfg[name]['yellow']:
+    elif value <= cfg[name]['thresholds']['yellow']:
         return yellow
     else:
         return red
 
 
-rTemp = requests.get(cfg['temperature']['api'], auth=(cfg['user'], cfg['password']))
-data = rTemp.json()
-print(data['result'])
+for metric in ('temperature', 'humidity'):
+    rTemp = requests.get(cfg[metric]['api'], auth=(cfg['user'], cfg['password']))
+    data = rTemp.json()
+    text_colour = getMetricColour(metric, data['result'])
+    if metric == 'temperature':
+        text_string = "T" + str(round(data['result'],1))
+    else:
+        text_string = "H" + str(round(data['result'],1))
+    payload = {
+        'back_colour': {
+            'r': 0,
+            'g': 0,
+            'b': 0
+        },
+        'text_colour': text_colour,
+        'text_string': text_string,
+        'scroll_speed': 0.1
+    }
+    r = requests.put(cfg['show_message']['api'], json=payload, auth=(cfg['user'], cfg['password']))
 
-rTemp = requests.get(cfg['humidity']['api'], auth=(cfg['user'], cfg['password']))
-data = rTemp.json()
-print(data)
-
-message=[]
-payload = {
-    'back_colour': {
-        'r': 0,
-        'g': 0,
-        'b': 0
-    },
-    'text_colour': {
-        'r': 255,
-        'g': 0,
-        'b': 0
-    },
-    'text_string': "Hola",
-    'scroll_speed': 0.07
-}
-
-string = json.dumps(payload)
-print(string)
-r = requests.put(cfg['show_message']['api'], json=payload, auth=(cfg['user'], cfg['password']))
-print(r.status_code)
-print(r.headers)
