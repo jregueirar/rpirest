848d5be362f310ee492568d3db05ca26316f0889
Jonás <jregueirar@gmail.com>
2018-06-09 13:40:04 +0100

    Finish First Version APIREST RPI

--- apirest_rpi/views.py
+++ apirest_rpi/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework import viewsets
 from core.common import MyRouter
 from core.common import apirest_response_format
+
 import psutil
 import logging
 
@@ -10,7 +11,9 @@ import logging
 def routes():
     router = MyRouter()
     router.register(r'cpu', CPUPercentView, base_name='rpi_cpu')
-    #router.register(r'memory', TemperatureView, base_name='dht_temperature')
+    router.register(r'memory', MemoryView, base_name='rpi_memory')
+    router.register(r'mountpoints', DiskPartitionView, base_name='rpi_disk_partition')
+    router.register(r'mountpoints-usage', DiskUsageView, base_name='rpi_disk_usage')
     return router.urls
 
 # Get an instance of a logger
@@ -23,9 +26,50 @@ class CPUPercentView(viewsets.ViewSet):
     First element of the list refers to first CPU, second element
     to second CPU and so on.
     """
-    # Device is from urls.py: am2302, dht11 or dht22
     def list(self, request):
 
         data=psutil.cpu_percent(interval=1, percpu=True)
         response = apirest_response_format(url=request.path, status="success", msg="Percents per CPU", result=data)
+        return Response(response)
+
+class MemoryView(viewsets.ViewSet):
+    """
+    Return statistics about system memory usage.
+    More info: http://psutil.readthedocs.io/en/latest/#memory
+    """
+    def list(self, request):
+
+        data=psutil.virtual_memory()
+        msg_out="Memory Statistics"
+        response = apirest_response_format(url=request.path, status="success", msg=msg_out, result=data._asdict())
+        return Response(response)
+
+class DiskPartitionView(viewsets.ViewSet):
+    """
+    Return all mounted disk partitions as a list of named tuples including device, mount
+    point and filesystem type, similarly to “df” command on UNIX
+    """
+    def list(self, request):
+        data=psutil.disk_partitions()
+        result=[]
+        for i in data:
+            result.append(i._asdict())
+        msgOut="List of all mounted partitions"
+        response = apirest_response_format(url=request.path, status="success", msg=msgOut, result=result)
+        return Response(response)
+
+class DiskUsageView(viewsets.ViewSet):
+    """
+    Return disk usage statistics of all mount points.
+    """
+    def list(self, request):
+        partitions = psutil.disk_partitions()
+        data = []
+        for partition in partitions:
+            aux = {}
+            aux[partition.mountpoint] = psutil.disk_usage(partition.mountpoint)._asdict()
+            data.append(aux);
+        # data=psutil.disk_usage(mount_point)._asdict()
+        msgOut="Use of mount points"
+        response = apirest_response_format(url=request.path, status="success", msg=msgOut, result=data)
         return Response(response)
